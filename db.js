15e4467d13167c38d332bdf662abb480a14ed119
Ayush Kumar <ak433778@gmail.com>
2025-08-10 17:30:08 +0530

    Update db.js

--- db.js
+++ db.js
@@ -7,12 +7,6 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  enableKeepAlive: true,
 });
-
-setInterval(() => pool.query("SELECT 1").catch(() => {}), 300000);
 
 module.exports = pool;
